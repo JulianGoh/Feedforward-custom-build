52fc759c48a76c64d19362b41955570684a1310d
Julian Goh <juliangoh5@gmail.com>
2021-10-07 14:33:55 +1000

    7/10/21 - Test on mobile

--- script.js
+++ script.js
@@ -291,12 +291,12 @@ function pad(val) {
 const wakeButton = document.querySelector('[data-status]');
 
 // change button and status if wakelock becomes aquired or is released
-const changeUI = (status = 'acquired') => {
-  const acquired = status === 'acquired' ? true : false;
-  wakeButton.dataset.status = acquired ? 'on' : 'off';
-  // wakeButton.textContent = `Turn Wake Lock ${acquired ? 'OFF' : 'ON'}`;
-  // statusElem.textContent = `Wake Lock ${acquired ? 'is active!' : 'has been released.'}`;
-}
+// const changeUI = (status = 'acquired') => {
+//   const acquired = status === 'acquired' ? true : false;
+//   wakeButton.dataset.status = acquired ? 'on' : 'off';
+//   // wakeButton.textContent = `Turn Wake Lock ${acquired ? 'OFF' : 'ON'}`;
+//   // statusElem.textContent = `Wake Lock ${acquired ? 'is active!' : 'has been released.'}`;
+// }
 
 // test support
 let isSupported = false;
@@ -318,23 +318,23 @@ if (isSupported) {
     try {
       wakeLock = await navigator.wakeLock.request('screen');
 
-      // change up our interface to reflect wake lock active
-      changeUI();
+      // // change up our interface to reflect wake lock active
+      // changeUI();
 
       // listen for our release event
       wakeLock.onrelease = function(ev) {
         console.log(ev);
       }
-      wakeLock.addEventListener('release', () => {
-        // if wake lock is released alter the button accordingly
-        changeUI('released');
-      });
+      // wakeLock.addEventListener('release', () => {
+      //   // if wake lock is released alter the button accordingly
+      //   // changeUI('released');
+      // });
 
     } catch (err) {
-      // if wake lock request fails - usually system related, such as battery
-      wakeButton.dataset.status = 'off';
-      wakeButton.textContent = 'Turn Wake Lock ON';
-      statusElem.textContent = `${err.name}, ${err.message}`;
+      // // if wake lock request fails - usually system related, such as battery
+      // wakeButton.dataset.status = 'off';
+      // wakeButton.textContent = 'Turn Wake Lock ON';
+      // statusElem.textContent = `${err.name}, ${err.message}`;
 
     }
   } // requestWakeLock()
@@ -342,15 +342,24 @@ if (isSupported) {
   // if we click our button
   wakeButton.addEventListener('click', () => {
     // if wakelock is off request it
-    if (wakeButton.dataset.status === 'off') {
+    // if (wakeButton.dataset.status === 'off') {
       requestWakeLock()
-    } else { // if it's on release it
-      wakeLock.release()
+    // } else { // if it's on release it
+    //   wakeLock.release()
+    //     .then(() => {
+    //       wakeLock = null;
+    //     })
+    // }
+  })
+
+  btnEnd.addEventListener('click', () => {
+    wakeLock.release()
         .then(() => {
           wakeLock = null;
         })
-    }
+    console.log('Wake lock stop');
   })
+
 
   const handleVisibilityChange = () => {
     if (wakeLock !== null && document.visibilityState === 'visible') {
